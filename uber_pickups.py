7a62d3ea0849958e19817c213ecde72bb4bea8b1
Thomas2512 <20924993+Thomas2512@users.noreply.github.com>
2020-02-13 10:33:04 +0100

    Added new testings of Streamlit features
    Added carré slider

--- uber_pickups.py
+++ uber_pickups.py
@@ -1,47 +1,73 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import time
 
-st.title('Uber pickups in NYC')
+st.title('pk nyc')
 
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
-
-rows_data = st.slider('Combien de data voulez-vous voir?', 1000, 20000)
+         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
 @st.cache
 def load_data(nrows):
     data = pd.read_csv(DATA_URL, nrows=nrows)
     lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
+    data.rename(lowercase, axis = 'columns', inplace=True)
     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
     return data
 
+
+n_to_load = st.slider('How much data do you want?', 1, 100000)
+
 data_load_state = st.text('Loading data...')
-data = load_data(rows_data)
-data_load_state.text('Loading data... done!')
+data = load_data(n_to_load)
+st.write('DONE !!! (using st.cache)')
 
 if st.checkbox('Show raw data'):
     st.subheader('Raw data')
     st.write(data)
 
 st.subheader('Number of pickups by hour')
-hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+hist_values = np.histogram(
+    data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+
 st.bar_chart(hist_values)
 
-# Some number in the range 0-23
-hour_to_filter = st.slider('hour', 0, 23, 17)
-filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+x = st.slider('Select a value')
+st.write(x, 'carré = ', x**2)
+# st.write(x, 'squared is', x * x)
+#
+st.subheader('Map of all pickups')
+st.map(data)
+hour_to_filter = 17
+st.write(f'{hour_to_filter}:00')
+filtered_data = data[data[DATE_COLUMN].dt.hour == 17]
 
-st.subheader('Map of all pickups at %s:00' % hour_to_filter)
-st.map(filtered_data)
+latitude = st.slider('LATIUDE', 40.5, 41.0)
+st.subheader(f'Map of all pickups above latitude: {latitude}')
+st.map(data[data['lat']>= latitude])
 
-lat_min = data['lat'].min()
-lat_max = data['lat'].max()
-st.write('Latitudes : \[', lat_min, ', ', lat_max, '\]')
+st.subheader('Map of all pickups at 16h')
+st.map(data[data[DATE_COLUMN].dt.hour == 16])
 
-latitude_filter = st.slider('Latitude minimale', lat_min, lat_max)
-st.subheader('Map of pickups above latitude %s' % latitude_filter)
-st.write(data[data['lat'] >= latitude_filter])
-st.map(data[data['lat'] >= latitude_filter])
+
+hour = st.slider('Hour', 0, 23)
+st.subheader('Map of all pickups at %s o\'clock' % hour)
+st.map(data[data[DATE_COLUMN].dt.hour == hour])
+
+st.write(x*3-2*x**2)
+
+'Starting a long computation...'
+
+# Add a placeholder
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+  # Update the progress bar with each iteration.
+  latest_iteration.text(f'Iteration {i+1}')
+  bar.progress(i + 1)
+  time.sleep(0.1)
+
+'...and now we\'re done!'
